fc1592d77ffd36498e3d880f37f649c28ec83fec
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2017-12-28 14:39:52 +0700

    update source code for OkHttp3 instead of OkHttp2. a sample relate to MockWebserver

--- app/src/main/java/com/hung/okhttptest/MainActivity.java
+++ app/src/main/java/com/hung/okhttptest/MainActivity.java
@@ -3,15 +3,6 @@ package com.hung.okhttptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.Headers;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
